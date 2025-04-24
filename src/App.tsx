9f4b084985253d0f833f2dad8f16e3e8e1da434e
Daniele Rangel <danielecrangel@outlook.com>
2025-04-24 13:18:31 -0300

    alterações no index.html,Footer e Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from './pages/home/Home';
 
 function App() {
   return (
-    <>
+    <Home>
       <Navbar />
       <Home />
       <Footer />
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 function Navbar() {
     return (
         <>
@@ -5,14 +7,23 @@ function Navbar() {
             			   bg-indigo-900 text-white'>
             
                 <div className="container flex justify-between text-lg">
-                    Blog Pessoal
-
+                <Link to='/home' className="text-2xl font-bold">People Flow</Link>
                     <div className='flex gap-4'>
-                        Postagens
-                        Temas
-                        Cadastrar tema
-                        Perfil
-                        Sair
+                    <Link to="/setor" className="text-indigo-800 hover:underline">
+                    Setor
+                    </Link>
+                    <Link to="/usuario" className="text-indigo-800 hover:underline">
+                    Usuário
+                    </Link>
+                    <Link to="/cadastro" className="text-indigo-800 hover:underline">
+                     Cadastro
+                    </Link>
+                    <Link to="/login" className="text-indigo-800 hover:underline">
+                    Login
+                    </Link>
+                    <Link to="/sair" className="text-indigo-800 hover:underline">
+                    Sair
+                    </Link>
                     </div>
                 </div>
             </div>
