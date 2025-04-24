9f7ab45d0f24a8d6f3a48f157e82c7bf00e7c081
Daniele Rangel <danielecrangel@outlook.com>
2025-04-24 19:13:45 -0300

    ajustes finais navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,12 @@
 import { Link, useNavigate } from "react-router-dom"
 import { AuthContext } from "../../contexts/AuthContext";
-import { useContext } from "react";
+import { ReactNode, useContext } from "react";
 
 function Navbar() {
     
     const navigate = useNavigate();
 
-    const { handleLogout } = useContext(AuthContext)
+    const { usuario, handleLogout } = useContext(AuthContext)
 
     function logout() {
 
@@ -14,8 +14,13 @@ function Navbar() {
         alert('O Usuário foi desconectado com sucesso!')
         navigate('/')
     }
-    return (
-        <>
+    
+    let component: ReactNode
+
+    if (usuario.token !== "") {
+
+        component = (
+
             <div className='w-full flex justify-center py-4
             			    bg-indigo-900 text-white'>
             
@@ -25,19 +30,19 @@ function Navbar() {
                     <div className='flex gap-4'>
                     <Link to="/setor" className="hover:underline">Setor</Link>
                     <Link to='/cadastrarsetor' className='hover:underline'>Cadastrar Setor</Link>
-                    <Link to="/usuario" className="text-indigo-800 hover:underline">
-                    Usuário
-                    </Link>
-                    <Link to="/cadastro" className="text-indigo-800 hover:underline">
-                    Cadastro
-                    </Link>
-                    <Link to="/login" className="text-indigo-800 hover:underline">
-                    Login
-                    </Link>
+                    <Link to="/usuario" className="hover:underline">Usuário</Link>
+                    <Link to='/perfil' className='hover:underline'>Perfil</Link>
+                    <Link to="/cadastro" className="hover:underline">Cadastro</Link>
+                    <Link to="/login" className="hover:underline">Login</Link>
                     <Link to='' onClick={logout} className='hover:underline'>Sair</Link>            
                     </div>
                 </div>
             </div>
+        )
+    }
+    return (
+        <>
+            { component }
         </>
     )
 }
