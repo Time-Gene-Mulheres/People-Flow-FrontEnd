855fc3a15f816a4e81f1d4224209588bccb3d2e7
Carolina <Caroliveira.almeida@gmail.com>
2025-04-25 10:30:37 -0300

    Atualizacoes da navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import { Link, useNavigate } from "react-router-dom"
 import { AuthContext } from "../../contexts/AuthContext";
 import { ReactNode, useContext } from "react";
+import { ToastAlerta } from "../../utils/ToastAlert";
 
 function Navbar() {
     
@@ -22,22 +23,24 @@ function Navbar() {
         component = (
 
             <div className='w-full flex justify-center py-4
-            			    bg-indigo-900 text-white'>
-            
-                <div className="container flex justify-between text-lg">
-                    <Link to='/home' className="text-2xl font-bold">People Flow</Link>
+            bg-indigo-900 text-white'>
 
-                    <div className='flex gap-4'>
-                    <Link to="/setor" className="hover:underline">Setor</Link>
-                    <Link to='/cadastrarsetor' className='hover:underline'>Cadastrar Setor</Link>
-                    <Link to="/usuario" className="hover:underline">Usuário</Link>
-                    <Link to='/perfil' className='hover:underline'>Perfil</Link>
-                    <Link to="/cadastro" className="hover:underline">Cadastro</Link>
-                    <Link to="/login" className="hover:underline">Login</Link>
-                    <Link to='' onClick={logout} className='hover:underline'>Sair</Link>            
-                    </div>
-                </div>
+        <div className="container flex justify-between text-lg">
+        <Link to='/home' className="text-2xl font-bold">People Flow</Link>
+
+            <div className='flex gap-4'>
+            <Link to="/setor" className="text-indigo-800 hover:underline">
+            Setor
+            </Link>
+            <Link to="/usuario" className="text-indigo-800 hover:underline">
+            Usuário
+            </Link>
+            <button onClick={logout} className="text-indigo-800 hover:underline">
+            Sair
+            </button>
             </div>
+        </div>
+    </div>
         )
     }
     return (
@@ -48,7 +51,3 @@ function Navbar() {
 }
 
 export default Navbar
-
-function ToastAlerta(arg0: string, arg1: string) {
-    throw new Error("Function not implemented.");
-}
