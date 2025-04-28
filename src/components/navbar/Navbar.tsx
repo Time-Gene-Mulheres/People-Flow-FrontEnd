6090fc805f8ffe84eec08865ba44e5ad1a43b54a
Daniele Rangel <danielecrangel@outlook.com>
2025-04-28 11:37:52 -0300

    fix button sair da navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -53,15 +53,12 @@ function Navbar() {
             <button className="bg-[#150425] text-white border border-[#5D2C73] border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
     <span className="bg-[#8D5ABF] shadow-[#8D5ABF] absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
     <div  className="flex flex-flow gap-2 items-center">
-          <Users size={20} /> 
+          <Users size={32} /> 
           <p>Colaborador</p> 
           </div>
             </button>
-
             </Link>
-            <button onClick={logout} className="text-white hover:underline">
-    
-            </button> <button className="bg-[#150425] text-white border border-[#5D2C73] border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
+    <button onClick={logout} className="bg-[#150425] text-white border border-[#5D2C73] border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
     <span className="bg-[#8D5ABF] shadow-[#8D5ABF] absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
     <div  className="flex flex-flow gap-2 items-center">
             <SignOut size={32} />
